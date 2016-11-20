0adaf72c7c4297190a999d99759f85cd74467036
Andrew Petersen <andrew.petersen15@gmail.com>
2016-11-20 17:32:18 -0600

    wunderground api

--- src/services/wunderground.js
+++ src/services/wunderground.js
@@ -1,4 +1,4 @@
-var apiKey = "ae55f9d6f8278139";
+var apiKey = "98f6d0949c244ff1";
 var request = require("request-promise-native");
 
 var baseUrl = "http://api.wunderground.com/api/" + apiKey;
@@ -7,12 +7,12 @@ exports.getCurrentConditions = function(city) {
     city = city.replace(/ /g,"_");
 
     var url = `${baseUrl}/conditions/q/WI/${city}.json`
-    return request({ url, json: true}).then(data => data.current_observation);
+    return request({ url, json: true}).then(data => data.current_observation).catch(e => console.log(e));
 }
 
 exports.getForecast = function(city) {
     city = city.replace(/ /g,"_");
 
     var url = `${baseUrl}/forecast/q/WI/${city}.json`
-    return request({ url, json: true}).then(data => data.forecast.txt_forecast.forecastday);
+    return request({ url, json: true}).then(data => data.forecast.txt_forecast.forecastday).catch(e => console.log(e));
 }
